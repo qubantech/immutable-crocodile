1df6a5f2c63c945aa6e2b5342201060b77e74b3e
v.shustov <57774085+shuimi@users.noreply.github.com>
2022-05-14 20:01:21 +0300

    -modules added

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
+import { SequelizeModule } from "@nestjs/sequelize";
+import { Contractor } from "../../models/contractor.model";
+import { Event } from "../../models/event.model";
 
 @Module({
   controllers: [EventsController],
-  providers: [EventsService]
+  providers: [EventsService],
+  imports: [SequelizeModule.forFeature([Event])],
 })
 export class EventsModule {}
